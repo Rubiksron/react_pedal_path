b95fd08095119ed4d030aab3bd5fa5fe2f4ec5db
Rubiksron <rubiksron@gmail.com>
2020-05-27 12:08:06 -0700

    react app up and running minimally.

---
+++ src/js/components/Form.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "",
+      message: "hell low whirled",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState(() => {
+      return {
+        value,
+      };
+    });
+  }
+
+  render() {
+    console.log("message: ", this.state.message);
+    return (
+      <form>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+}
+
+export default Form;
+
+const wrapper = document.getElementById("container");
+wrapper ? ReactDOM.render(<Form />, wrapper) : false;
