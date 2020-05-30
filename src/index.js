ff03c70bd7913cdf485df2d283a2de0ceb94b8a8
Rubiksron <rubiksron@gmail.com>
2020-05-30 10:31:11 -0700

    changed npm start script from development to deployed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//to run in development comment out lines 6-12 and show line 4 and run 'npm start'
+//to run with the in-file server, comment out line 4 and uncomment 6-12 and run 'nodemon src/index.js'
+
 // import App from "./js/components/App";
 
 const express = require("express");
